0885ddad9135fada256c48bb3c8eb4140e6e43bc
tech@droommedia.com <tech@droommedia.com>
2018-10-17 23:53:24 +0530

    Updates

---
+++ precache-manifest.da5eca0754803f094a5a9eab6fe3a442.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cd2fbf0aa09584c7e724",
+    "url": "/static/js/main.cd2fbf0a.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "cd2fbf0aa09584c7e724",
+    "url": "/static/css/main.1da89def.chunk.css"
+  },
+  {
+    "revision": "400a2814f29c439bb0b0257483481886",
+    "url": "/index.html"
+  }
+];
